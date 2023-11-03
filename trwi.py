3ee27ad0c0797810c5c15cc3fc5c11091f6ac711
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2023-11-04 03:55:10 +0900

    Added some type hints and some comments

--- trwi.py
+++ trwi.py
@@ -6,7 +6,7 @@ import argparse
 from ipwhois import IPWhois
 
 
-def run_traceroute(destination):
+def run_traceroute(destination: str) -> str: 
     try:
         result = subprocess.run(
             ["traceroute", destination],
@@ -23,12 +23,12 @@ def run_traceroute(destination):
         return f"An error occurred: {str(e)}"
 
 
-def extract_ip_addresses(traceroute_output):
+def extract_ip_addresses(traceroute_output: str) -> list[str]:
     # Regular expression pattern for matching IP addresses
     ip_address_pattern = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"
-    ip_addresses = re.findall(ip_address_pattern, traceroute_output)
+    ip_addresses: list[str] = re.findall(ip_address_pattern, traceroute_output)
 
-    unique_ip_addresses = []
+    unique_ip_addresses: list[str] = []
     seen = set()
     for ip in ip_addresses:
         if ip not in seen:
@@ -38,23 +38,23 @@ def extract_ip_addresses(traceroute_output):
     return unique_ip_addresses
 
 
-def get_whois_info(ip_address):
+def get_whois_info(ip_address: str) -> dict[str, None] | str:
     try:
         return IPWhois(ip_address).lookup_whois()
     except Exception as e:
-        return f"Error: {str(e)}"
+        return f"Error: {str(e)}" 
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Retrieve WHOIS information for an IP address."
+        description="Print Ip Addresses and their respective organization of each IP passes through traceroute"
     )
     parser.add_argument(
-        "-i", dest="ip_address", required=True, help="IP address to lookup using WHOIS"
+        "-i", dest="ip_address", required=True, help="IP address to lookup"
     )
 
     args = parser.parse_args()
-    ip_address = args.ip_address
+    ip_address: str = args.ip_address
 
     traceroute_output = run_traceroute(ip_address)
     ip_addresses = extract_ip_addresses(traceroute_output)
@@ -63,6 +63,8 @@ def main():
         whois_info = get_whois_info(ip)
         if isinstance(whois_info, dict):
             print(f'{ip}: {whois_info["asn_description"]}')
+        else:
+            print("Something went wrong.")
 
 
 if __name__ == "__main__":
