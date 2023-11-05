5962e78c351ec91912f5fbbd7a981de3cf6b09f9
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2023-11-06 02:31:33 +0900

    Trying to implement traceroute in c

---
+++ trwi.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+int main(){
+    int sockfd,retval,n;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+    char buf[10000]; 
+    int i;
+
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0){
+        perror("sock:");
+        exit(1);
+    }
+
+    clilen = sizeof(struct sockaddr_in);    
+    while(1){
+        printf(" before recvfrom\n");   
+        n=recvfrom(sockfd,buf,10000,0,(struct sockaddr *)&cliaddr,&clilen);
+        printf(" rec'd %d bytes\n",n);
+
+        struct ip *ip_hdr = (struct ip *)buf;
+        printf("IP header is %d bytes.\n", ip_hdr->ip_len*4);
+        char targetaddr_str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &ip_hdr->ip_dst.s_addr, targetaddr_str, INET_ADDRSTRLEN);
+        printf("Target IP is %s\n", targetaddr_str);
+
+        for (i = 0; i < n; i++) {
+            printf("%02X%s", (uint8_t)buf[i], (i + 1)%16 ? " " : "\n");
+        }
+        printf("\n");
+        
+        struct icmp *icmp_hdr = (struct icmp *)((char *)ip_hdr + (4 * ip_hdr->ip_len));
+        printf("ICMP msgtype=%d, code=%d", icmp_hdr->icmp_type, icmp_hdr->icmp_code);
+    }
+
+    return 0;
+}
